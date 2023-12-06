e243476502ac11b43555547a02b15c2bda39ac03
ZhunC <95588119+ZhunC@users.noreply.github.com>
2023-12-06 17:17:52 -0500

    main folder with .h files

--- OpenManip_Skeleton/OpenManip_Skeleton-main/lab1_base.m
+++ OpenManip_Skeleton/OpenManip_Skeleton-main/lab1_base.m
@@ -1,10 +1,11 @@
 %% Setup robot
+addpath('C:\Users\bobby\Desktop\fl_23\rbe_501_final\OpenManip_Skeleton-main')
+addpath('C:\Users\bobby\Desktop\fl_23\rbe_501_final\DynamixelSDK-master')
 travelTime = 2; % Defines the travel time
 robot = Robot(); % Creates robot object
 robot.writeTime(travelTime); % Write travel time
 robot.writeMotorState(true); % Write position mode
 %% Program 
-
 robot.writeJoints(0); % Write joints to zero position
 pause(travelTime); % Wait for trajectory completion
 
